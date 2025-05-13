b727085fc77d73482cbac4ddc09bb09c9994f7d0
devalexsantos <devalexsantos@gmail.com>
2025-05-13 13:22:44 +0000

    chore: add link on cta button to whatsapp

--- app/components/CtaButton.tsx
+++ app/components/CtaButton.tsx
@@ -8,7 +8,11 @@ interface CtaButtonProps {
 
 export function CtaButton({ text, textStyle }: CtaButtonProps) {
   return (
-    <Link href="" className={`text-center w-full bg-[#009407] px-6 py-4 flex items-center justify-center gap-2 text-white text-2xl font-bold rounded-[24px]`}>
+    <Link
+      href="https://api.whatsapp.com/send?phone=5571996071970&text=Ol%C3%A1%2C%20acabei%20de%20ver%20o%20site%20de%20voc%C3%AAs%20e%20queria%20tirar%20uma%20d%C3%BAvida."
+      className={`text-center w-full bg-[#009407] px-6 py-4 flex items-center justify-center gap-2 text-white text-2xl font-bold rounded-[24px]`}
+      target="_blank"
+    >
       <Pointer className="w-8 h-8 rotate-90" />
       <span className={textStyle}>
         {text}
